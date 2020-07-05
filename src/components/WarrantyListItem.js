5062b72fd55633ea2e3b82449c80826aa9e6ffab
Forlify <forlify@gmail.com>
2020-07-05 10:43:55 +0200

    word breaking issue resolved

--- src/components/WarrantyListItem.js
+++ src/components/WarrantyListItem.js
@@ -7,12 +7,12 @@ const WarrantyListItem = ({id, category, number, item, model, serialNumber, star
         <th>
             <Link to={`/edit/${id}`}><img src="/images/edit.svg" className="svg"/></Link>
         </th>
-        <th>{category === 'buy' ? 'Kupno' : 'Sprzedaż'}</th>
+        <th className="list__unbreakable">{category === 'buy' ? 'Kupno' : 'Sprzedaż'}</th>
         <th>{number}</th>
         <th>{item}</th>
         <th>{model}</th>
         <th>{serialNumber}</th>
-        <th>{moment(startDate).format('DD-MM-YYYY')}</th>
+        <th className="list__unbreakable">{moment(startDate).format('DD-MM-YYYY')}</th>
         <th>{duration}</th>
         <th>{invoice}</th>
         <th>{other}</th>
